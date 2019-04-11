9be370f8ebc174cea7dd93fd1a8c6a1d943bbff2
AlbertoVf <albertoVf97@protonmail.com>
2019-04-11 21:51:12 +0200

    Traduccion español y limpieza de código

--- pdfjs-media-button.js
+++ pdfjs-media-button.js
@@ -1,21 +1,19 @@
 jQuery(function($) {
     $('#insert-pdfjs').click(openMediaWindow);
-
     function openMediaWindow() {
     	console.log('pdfjs media button clicked');
     	var frame = wp.media({
-            title: 'Insert a PDF',
+            title: 'Insertar un PDF',
             library: {type: 'application/pdf'},
             multiple: false,
-            button: {text: 'Insert'}
+            button: {text: 'Insertar'}
         });
 
         frame.on('select', function(){
 	        var selectionURL = frame.state().get('selection').first().toJSON().url;
 	        selectionURL = encodeURIComponent(selectionURL);
-	        wp.media.editor.insert('[pdfjs-viewer url="' + selectionURL + '" viewer_width=100% viewer_height=1360px fullscreen=true download=true print=true]');
+	        wp.media.editor.insert('[pdfjs-viewer url="' + selectionURL + ']');
 	    });
-
 	    frame.open();
     }
 });
