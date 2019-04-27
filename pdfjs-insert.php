2d58994f3c40e1ab921b4962ae720630b7f781d7
AlbertoVf <albertoVf97@protonmail.com>
2019-04-28 01:06:50 +0200

    Create pdfjs-insert.php
    Formulario de seleccion de guardado, nombre de fichero y ruta

---
+++ pdfjs-insert.php
@@ -0,0 +1,65 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Subir Pdf</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script>
+        $(() => {
+            $('#listas').on('change', () => {
+                $('.alert').attr('hidden', true);
+                var nombre = $('select#listas option:checked').val();
+                $('#file-name').attr('value', nombre);
+                (nombre != ' ') ? $('#aceptar').attr('hidden', false): $('#aceptar').attr('hidden', true);
+            });
+        });
+    </script>
+</head>
+
+<body>
+    <div id="container" class="container p-4">
+        <form id="form" action="index.php" method="POST" enctype="multipart/form-data">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="listas">Lista</label>
+                </div>
+                <select class="custom-select" id="listas">
+                    <option selected value=" ">Selecciona...</option>
+                    <option value="N1">Nombre 1</option>
+                    <option value="N2">Nombre 2</option>
+                </select>
+            </div>
+            <!-- Seleccionar un fichero para subir -->
+            <div class="input-group mb-3">
+                <div class="custom-file">
+                    <input id="archivo" class="form-control-file" type="file" name="archivo" accept=".pdf">
+                </div>
+            </div>
+            <!-- Cambiar el nombre. nombre con el que se guardara -->
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Nuevo nombre</span>
+                </div>
+                <input type="text" name="filename" class="form-control" id="file-name" readonly>
+            </div>
+            <!-- Carpeta para almacenar los pdf de una categoria/subcategoria-->
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="categorias-select">Selecciona una Categoria</label>
+                </div>
+                <select class="custom-select" id="categorias-select" name="categorias-select">
+                    <?php include_once('pdfjs-rutas.php');
+                    seleccionar_almacenamiento($ruta_docs); ?>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-success" id="aceptar" hidden>Aceptar</button>
+        </form>
+    </div>
+</body>
+
+</html>
