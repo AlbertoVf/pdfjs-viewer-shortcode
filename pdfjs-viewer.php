ea1b58b9bc55c00de89ee76808d3424d1d55c605
AlbertoVf <AlbertoVf97@protonmail.com>
2019-04-30 17:15:29 +0200

    Modal

--- pdfjs-viewer.php
+++ pdfjs-viewer.php
@@ -1,3 +1,21 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+    $(() => {
+        $('#listas').on('change', () => {
+            $('.alert').attr('hidden', true);
+            var nombre = $('select#listas option:checked').val();
+            $('#file-name').attr('value', nombre);
+            (nombre != ' ') ? $('#aceptar').attr('hidden', false): $('#aceptar').attr('hidden', true);
+        });
+    });
+</script>
+
+
+
+
 <?php
 /*
  * Plugin Name: PDF Viewer
@@ -10,7 +28,7 @@
  */
 
 //==== Shortcode ====
-include_once 'lib/pdfjs-rutas.php';
+include_once('lib/pdfjs-rutas.php');
 
 add_shortcode("pdfjs-viewer", "pdfjs_handler");
 global $carpeta_docs;
@@ -18,7 +36,6 @@ $root_pdf = Site_url() . $carpeta_docs;
 function pdfjs_handler($incoming_from_post)
 {
     $incoming_from_post = shortcode_atts(array('url' => 'bad-url.pdf', 'viewer_height' => '1360px', 'viewer_width' => '100%', 'fullscreen' => 'true', 'download' => 'true', 'print' => 'true', 'openfile' => 'false'), $incoming_from_post);
-
     $pdfjs_output = pdfjs_generator($incoming_from_post);
     return $pdfjs_output;
 }
@@ -34,7 +51,6 @@ function pdfjs_generator($incoming_from_handler)
     if ($download != 'true') $download = 'false';
     if ($print != 'true') $print = 'false';
     if ($openfile != 'true') $openfile = 'false';
-
     $final_url = str_replace(' ', '-', $file_name);
     $fullscreen_link = '';
     if ($fullscreen == 'true') $fullscreen_link = '<a class="pdf-pdfjs" href="' . $final_url . '">Ver</a><br>';
@@ -43,10 +59,60 @@ function pdfjs_generator($incoming_from_handler)
 
 //add_action('media_buttons', 'pdfjs_media_button', 12); //boton en pagina
 add_action('after_setup_theme', 'pdfjs_media_button');
+//BUG: la vista se visualiza en todas las paginas. Modificar para que solo se vea en Back-end;
 function pdfjs_media_button()
 {
-    echo '<div class="wp-menu-name" style="text-align:right; margin:0.5% 20px;">
-            <a href="' . plugins_url() . '/pdfjs-viewer-shortcode/lib/pdfjs-insert.php" target="_target" id="insert-pdfjs" class="button">PDF Viewer</a>
-        </div>';
+    // echo '<div class="wp-menu-name" style="text-align:right; margin:0.5% 20px;"> <a href="' . plugins_url() . '/pdfjs-viewer-shortcode/lib/pdfjs-insert.php" target="_target" id="insert-pdfjs" class="button">PDF Viewer</a> </div>';
+
+    echo ('
+    <div class="mt-2 mr-4 mb-2 ml-2 d-flex justify-content-end">
+        <button type="button" class="btn btn-light btn-sm" data-toggle="modal" data-target="#myModal">PDF Viewer</button>
+        <div class="modal" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Subir PDF</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body" id="container">
+                        <form id="form" action="lib/sobreescribir/index.php" method="POST" enctype="multipart/form-data">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="listas">Lista</label>
+                                </div>
+                                <select class="custom-select" id="listas">
+                                    <option selected value=" ">Selecciona...</option>
+                                    <option value="LP_admitidos">Lista provisional de admitidos</option>
+                                    <option value="LD_admitidos">Lista definitiva de admitidos</option>
+                                    <option value="LP_seleccionados">Lista provisional de seleccionados</option>
+                                    <option value="LD_seleccionados">Lista definitiva de seleccionados</option>
+                                </select>
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="custom-file">
+                                    <input id="archivo" class="form-control-file" type="file" name="archivo" accept=".pdf">
+                                </div>
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend"> 
+                                    <span class="input-group-text">Nuevo nombre</span> 
+                                </div>
+                                <input type="text" name="filename" class="form-control" id="file-name" readonly>
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="categorias-select">Selecciona una Categoria</label>
+                                </div>
+                                <select class="custom-select" id="categorias-select" name="categorias-select">
+                                    <?php include_once("lib/pdfjs-rutas.php"); seleccionar_almacenamiento($ruta_docs); ?>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-success" id="aceptar" hidden>Aceptar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>');
 }
 ?>
